93ea1bba48a3420123cb882dd2f02fef4c3247dd
David Indra <mail@davidindra.cz>
2016-11-12 18:40:01 +0100

    Move

--- loader.js
+++ loader.js
@@ -36,7 +36,7 @@ class Predictor {
 
     finishSetup(active = false) {
         $('body').append(
-            '<img id="pr-logo"' + (!active ? ' class="grayscale"' : '') + '" src="//predvidac.davidindra.cz/icon-48.png" data-ot="<b>Bakaláři 2</b> ' + (active ? 'aktivní' : 'neaktivní') + '<br>&copy; David Indra">'
+            '<img style="display: none;" id="pr-logo"' + (!active ? ' class="grayscale"' : '') + '" src="//predvidac.davidindra.cz/icon-48.png" data-ot="<b>Bakaláři 2</b> ' + (active ? 'aktivní' : 'neaktivní') + '<br>&copy; David Indra">'
         );
     }
 
