6310ffc99f7e9fbf03ecc6042f718cadc2f9e5d4
David Indra <mail@davidindra.cz>
2016-11-13 19:52:10 +0100

    Test

--- loader.js
+++ loader.js
@@ -103,6 +103,9 @@ class Predictor {
                     `<div data-ot="
                     ${earnedTotal} / ${maximumTotal}<br>
                     1: ${g1[0] == 'more' ? g1[1] : '0'}/${g1[1]}<br>
+                    2: ${g2[0] == 'more' ? g2[1] : '0'}/${g2[1]}<br>
+                    3: ${g3[0] == 'more' ? g3[1] : '0'}/${g3[1]}<br>
+                    4: ${g4[0] == 'more' ? g4[1] : '0'}/${g4[1]}
                     " class="detprumerdiv pr-grade-${predictor.grade(ratioTotal)}">
                     ${(maximumTotal == 0 ? '-' : (ratioTotal + '% (' + predictor.grade(ratioTotal) + ')'))}
                     </div>`
